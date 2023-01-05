ede3a6150118e440ef0db4f017e9084e751fca18
Minguk Gaang <minguk.gaang@gmail.com>
2023-01-05 22:45:27 +0100

    start to get logtime

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-async function getDataFromAPI() {
+async function getToken() {
 	try {
 		const response = await fetch('https://api.intra.42.fr/oauth/token', {
 			method: 'POST',
@@ -8,29 +8,60 @@ async function getDataFromAPI() {
 			body: `grant_type=client_credentials&client_id=${UID}&client_secret=${SECRET}`,
 		});
 		const data = await response.json();
-		const token = data.access_token;
+		return (data.access_token);
+	} catch (e) {
+		console.error(e);
+	}
+}
 
+async function getCampusUsers(token) {
+	try {
 		let pageNb = 0;
 		let allCampusUsers = [];
 		while (++pageNb) {
 			const params = new URLSearchParams();
-			params.set('filter[campus_id]', 30);
-			params.set('page[number]', pageNb)
+			params.set('filter[kind]', 'student');
+			params.set('page[number]', pageNb - 1);
 			params.set('page[size]', 100);
-			const apiResponse = await fetch('https://api.intra.42.fr/v2/campus_users/?${params}', {
+			const apiResponse = await fetch(`https://api.intra.42.fr/v2/campus/41/users?&${params.toString()}`, {
 				method: 'GET',
 				headers: { Authorization: `Bearer ${token}`, },
 			});
 			const apiData = await apiResponse.json();
-			if (!apiData.length)
+			if (!apiData.length) {
+				console.error("hello?");
 				break ;
+			}
 			allCampusUsers = allCampusUsers.concat(apiData);
-			console.log(pageNb, allCampusUsers);
+			console.log(pageNb - 1, allCampusUsers.length);
 			await new Promise(resolve => setTimeout(resolve, 500));
 		}
+		return (allCampusUsers);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function getDataFromAPI() {
+	try {
+		const token = await getToken();
+		// // const allCampusUsers = await getCampusUsers(token);
+		
+		// console.log(allCampusUsers[0]);
+		
+		const params = new URLSearchParams();
+		params.set('begin_at', '2022-01-01');
+		params.set('end_at', '2022-12-31');
+		const apiResponse = await fetch(`https://api.intra.42.fr/v2/users/min-kang/locations_stats?&${params.toString()}`, {
+			method: 'GET',
+			headers: { Authorization: `Bearer ${token}`, },
+		});
+		const apiData = await apiResponse.json();
+		console.log(apiData);
 	} catch (error) {
 		console.error(error);
 	}
 }
+	
 
 getDataFromAPI();
